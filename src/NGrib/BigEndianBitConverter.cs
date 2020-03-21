321d3ba6f00f70619a39efa14262c5010da78e30
Nicolas MANGUÉ <nicolas.mangue@outlook.com>
2020-03-21 15:09:46 +0100

    Add BigEndianBitConverter

---
+++ src/NGrib/BigEndianBitConverter.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Numerics;
+
+namespace NGrib
+{
+	internal static class BigEndianBitConverter
+	{
+		public static int ToUInt16(byte[] data, int startIndex)
+		{
+			var index = startIndex;
+			return (data[index++] << 8) | data[index];
+		}
+		
+		public static long ToUInt32(byte[] data, int startIndex)
+		{
+			var index = startIndex;
+			return ((uint) data[index++] << 24) |
+			       ((uint) data[index++] << 16) |
+			       ((uint) data[index++] <<  8) |
+			       data[index];
+		}
+		
+		public static BigInteger ToUInt64(byte[] data, int startIndex)
+		{
+			var index = startIndex;
+			return ((ulong) data[index++] << 56) |
+			       ((ulong) data[index++] << 48) |
+			       ((ulong) data[index++] << 40) |
+			       ((ulong) data[index++] << 32) |
+			       ((ulong) data[index++] << 24) |
+			       ((ulong) data[index++] << 16) |
+			       ((ulong) data[index++] <<  8) |
+			       data[index];
+		}
+
+		public static float ToSingle(byte[] data, int startIndex)
+		{
+			if (BitConverter.IsLittleEndian)
+			{
+				var reversedBytes = new[]
+				{
+					data[startIndex+3],
+					data[startIndex+2],
+					data[startIndex+1],
+					data[startIndex]
+				};
+				return BitConverter.ToSingle(reversedBytes, 0);
+			}
+			return BitConverter.ToSingle(data, startIndex);
+		}
+	}
+}
---
+++ tests/NGrib.Tests/BigEndianBitConverterTest.cs
@@ -0,0 +1,54 @@
+﻿using System.Numerics;
+using NFluent;
+using Xunit;
+
+namespace NGrib.Tests
+{
+	public class BigEndianBitConverterTest
+	{
+		[Theory]
+		[InlineData(new byte[] { 0, 13 }, 0, 13)]
+		[InlineData(new byte[] { 0, 255 }, 0, 255)]
+		[InlineData(new byte[] { 0x7, 0xe4 }, 0, 2020)]
+		[InlineData(new byte[] { 0xff, 0x7, 0xfa }, 1, 2042)]
+		[InlineData(new byte[] { 0xff, 0xff }, 0, 65535)]
+		public void ToUInt16_Test(byte[] data, int startIndex, int value)
+		{
+			Check.That(BigEndianBitConverter.ToUInt16(data, startIndex)).Equals(value);
+		}
+		
+		[Theory]
+		[InlineData(new byte[] { 0, 0, 0, 13 }, 0, 13)]
+		[InlineData(new byte[] { 0x24, 0x80, 0xc6, 0xd3 }, 0, 612419283)]
+		[InlineData(new byte[] { 0xff, 0xff, 0xff, 0xff }, 0, 4294967295)]
+		public void ToUInt32_Test(byte[] data, int startIndex, long value)
+		{
+			Check.That(BigEndianBitConverter.ToUInt32(data, startIndex)).Equals(value);
+		}
+		
+		[Theory]
+		[MemberData(nameof(UInt64TestData))]
+		public void ToUInt64_Test(byte[] data, int startIndex, BigInteger value)
+		{
+			Check.That(BigEndianBitConverter.ToUInt64(data, startIndex)).Equals(value);
+		}
+		
+		public static object[][] UInt64TestData = 
+		{
+			new object[] {new byte[] { 0, 0, 0, 0, 0, 0, 0, 13 }, 0, new BigInteger(13) },
+			new object[] {new byte[] { 0x2a, 0xcc, 0, 0, 0, 0xb, 0x02, 0x54, 0x4c, 0x46, 0 }, 2, new BigInteger(47283719238) },
+			new object[] {new byte[] { 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff }, 0, new BigInteger(18446744073709551615) },
+		};
+
+
+		[Theory]
+		[InlineData(new byte[] { 74, 143, 11, 0 }, 0, 4687232f)]
+		[InlineData(new byte[] { 69, 28, 5, 204 }, 0, 2496.3623f)]
+		[InlineData(new byte[] { 0, 1, 2, 69, 12, 38, 226, 0 }, 3, 2242.43018f)]
+		public void ToSingle_Test(byte[] data, int startIndex, float value)
+		{
+			Check.That(BigEndianBitConverter.ToSingle(data, startIndex)).Equals(value);
+		}
+
+	}
+}
