b53a202515cf70b03b43643d24f3429d8cb2066a
Nicolas MANGUÉ <nicolas.mangue@gmail.com>
2020-03-24 13:44:51 +0100

    Add new grid definition

---
+++ src/NGrib/Sections/Templates/GridDefinitionTemplates/0031_AlbersEqualAreaGridDefinition.cs
@@ -0,0 +1,9 @@
+﻿namespace NGrib.Sections.Templates.GridDefinitionTemplates
+{
+    public class AlbersEqualAreaGridDefinition : LambertConformalGridDefinition
+    {
+        internal AlbersEqualAreaGridDefinition(BufferedBinaryReader reader) : base(reader)
+        {
+        }
+    }
+}
